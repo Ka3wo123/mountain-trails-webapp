660022f0448be17c27f7fe079d2e25a24c414b04
Kajetan Nowak <kajtek2n@gmail.com>
2025-02-28 16:47:35 +0100

    Changes

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -4,7 +4,7 @@ const BASE_URL = import.meta.env.VITE_PROD_URL || 'http://localhost:5000/api';
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
-    // withCredentials: true,
+    withCredentials: true,
     headers: {
         "Content-Type": "application/json"
     }
